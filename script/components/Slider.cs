33175d611ce55d9fece1a8430b17e158d7a206d3
ppisod <hicnkebromzih@gmail.com>
2025-12-29 15:44:21 +0800

    M  snaresJ.csproj Slider.cs sliderTrack.tscn

--- script/components/Slider.cs
+++ script/components/Slider.cs
@@ -20,8 +20,15 @@ public partial class Slider : MarginContainer {
 
 	public int LMarg = 10;
 	public int RMarg = 10;
-	public int maxHeight = 50;
+	public int maxHeight = 0;
 	public float ERatio = 1f;
+
+	// introduce anim constants
+	public const int goalHeight = 50;
+	public const double time = 0.5;
+
+	// time
+	public double elapsed = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
@@ -30,8 +37,16 @@ public partial class Slider : MarginContainer {
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
+		elapsed += delta;
+		if (elapsed < time)
+		{
+			maxHeight = (int) Single.Lerp ( 0, goalHeight, (float) elapsed / (float) time );
+		}
+		else
+		{
+			maxHeight = goalHeight;
+		}
 		// use Size vector to determine how much padding to apply
 		fixMargin( );
 	}
