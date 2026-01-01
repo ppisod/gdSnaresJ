2ba8428f0c7217a154a29705614be3e09d496892
ppisod <hicnkebromzih@gmail.com>
2026-01-01 19:02:13 +0800

    .DS_Store DebugMenu.cs DebugMenu.cs.uid M  .DS_Store center.png center.png.import debugMenu.tscn game.tscn hihat.png hihat.png.import left.png left.png.import right.png right.png.import snare.png snare.png.import ticker.aseprite ticker.png ticker.png.import

---
+++ script/components/DebugMenu.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class DebugMenu : Control {
+
+	public bool wronged = false;
+	public bool initialized = false;
+	public Game game = null!;
+	public RichTextLabel label = null!;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+
+		var gaem = GetTree ().GetCurrentScene ();
+
+		if (!(gaem is Game))
+		{
+			GD.PrintErr ( "DebugMenu should only be in Game scene!" );
+			wronged = true;
+		}
+
+		if (!wronged)
+		{
+			var gmem = gaem as Game;
+			game = gmem;
+		}
+
+		label = GetNode<RichTextLabel> ("highbox/container/debugtext");
+		if (label == null) GD.PrintErr ( "DebugMenu should have RichTextLabel named 'debugtext'" );
+
+		initialized = true;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) {
+		if (wronged || !initialized) return;
+		
+	}
+}
