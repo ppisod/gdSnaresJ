52b1b721e1fee67b5f251f6c66cd8d6d72f01e7f
ppisod <hicnkebromzih@gmail.com>
2026-01-01 21:53:30 +0800

    GameProcessing.cs

--- script/scene/GameProcessing.cs
+++ script/scene/GameProcessing.cs
@@ -1,29 +1,35 @@
+using Godot;
 using snaresJ.script.beatmaps.Events;
 
 public partial class Game {
-    public void ProcessBeatmapEvents ( ) {
-        var currentBeatD = metronome.GetCurrentTotalBeats ();
-        foreach (TimelyEvent sceneEvent in BeatmapEvents.sceneEvents.GetNext ( BeatmapEvents.sceneEventPollLimit ))
-        {
-            if (!sceneEvent.hasPassedBeat ( currentBeatD )) continue;
-            if (sceneEvent is StartDisplayingTrack SDTEvent) sliders.AddTrackToScene ( SDTEvent.track );
-            if (sceneEvent is IntroduceTrack ITEvent)
-                sliders.sliders.Find ( track => track.track == ITEvent.GetTrackObject () ).Active = true;
-        }
+	public void ProcessBeatmapEvents ( ) {
+		var currentBeatD = metronome.GetCurrentTotalBeats ();
+		foreach (TimelyEvent sceneEvent in BeatmapEvents.sceneEvents.GetNext ( BeatmapEvents.sceneEventPollLimit ))
+		{
+			if (sceneEvent is null)
+			{
+				GD.PrintErr ( "scene event is null???!?!??" );
+				continue;
+			}
+			if (!sceneEvent.hasPassedBeat ( currentBeatD )) continue;
+			if (sceneEvent is StartDisplayingTrack SDTEvent) sliders.AddTrackToScene ( SDTEvent.track );
+			if (sceneEvent is IntroduceTrack ITEvent)
+				sliders.sliders.Find ( track => track.track == ITEvent.GetTrackObject () ).Active = true;
+		}
 
-        foreach (TimelyEvent rhythmObject in BeatmapEvents.rhythmObjects.GetNext (
-                     BeatmapEvents.rhythmObjectPollLimit
-                 ))
-        {
-            if (rhythmObject is Snare snare && snare.hasPassedBefore ( currentBeatD, metronome.Numerator ))
-            {
-                // TODO: introduce the snare to the track
-            }
+		foreach (TimelyEvent rhythmObject in BeatmapEvents.rhythmObjects.GetNext (
+					 BeatmapEvents.rhythmObjectPollLimit
+				 ))
+		{
+			if (rhythmObject is Snare snare && snare.hasPassedBefore ( currentBeatD, metronome.Numerator ))
+			{
+				// TODO: introduce the snare to the track
+			}
 
-        }
-    }
+		}
+	}
 
-    public void ProcessUserInput ( ) {
+	public void ProcessUserInput ( ) {
 
-    }
+	}
 }
